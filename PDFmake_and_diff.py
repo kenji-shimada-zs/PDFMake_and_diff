df1abf87fe854dbb5a88f455d5227423426a535a
kenji_shimada <kenji.shimada.zs@kyocera.jp>
2025-01-23 15:37:28 +0900

    ネットから落としてきたdiff-PDFを追加

--- PDFmake_and_diff.py
+++ PDFmake_and_diff.py
@@ -1,6 +1,6 @@
 import diff_pdf as diff
 import PDFMerge as PDFMerge
-import pathlib,os
+import pathlib,os,subprocess,re,sys
 
 UserDrive =pathlib.Path(f"{os.getenv('USERPROFILE')}")
 Onedrive  =UserDrive.joinpath("OneDrive - 京セラ株式会社")
@@ -18,9 +18,22 @@ os.makedirs(tmp,exist_ok=True)
 PDFMerge.pdfMerge(sht_dir=sht,out_dir=out,tmp_dir=tmp)
 
 PDF_files=list(out.glob('*.pdf'))
+PDF_files=[p for p in PDF_files if re.search(pattern='[0-9]{8}_[0-9]{4}.pdf',string=str(p))]
 if len(PDF_files) == 0:
     print('Can not find pdf files')
-print(f'old_PDF_file=[{PDF_files[-2]}]')
-print(f'new_PDF_file=[{PDF_files[-1]}]')
-diff.diff(pdf_old_path=PDF_files[-2],pdf_current_path=PDF_files[-1],output_dir=out,temp_addr=tmp)
-print('Finished')    
+def make_diff(old_PDF_file:pathlib.Path,new_PDF_file:pathlib.Path,out_diff_file:pathlib.Path):
+    print(f'old_PDF_file={old_PDF_file}')
+    print(f'new_PDF_file={new_PDF_file}')
+    subprocess.run(f'diff-pdf --output-diff="{out_diff_file}" "{old_PDF_file}" "{new_PDF_file}"')
+    #diff.diff(pdf_old_path=PDF_files[-2],pdf_current_path=PDF_files[-1],output_dir=out,temp_addr=tmp)
+
+for i in range(1,len(PDF_files)):
+    old_PDF_file=PDF_files[i-1]
+    new_PDF_file=PDF_files[i  ]
+    output_file_name=f'{old_PDF_file.stem}-{new_PDF_file.stem}_diff.pdf'
+    output_file_path=out.joinpath(output_file_name)
+    if output_file_path.exists() == False:
+        make_diff(old_PDF_file=old_PDF_file,new_PDF_file=new_PDF_file,out_diff_file=output_file_path)
+
+print('Finished')
+sys.exit()
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # build
 ## command
-    pyinstaller PDFmake_and_diff.py Marge_PDF_page.py --onefile --exclude pip
+    pyinstaller PDFmake_and_diff.py Marge_PDF_page.py --onefile --exclude pip --noconsole
